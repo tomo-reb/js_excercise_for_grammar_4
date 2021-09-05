81424a6bdf2ddb822cbbf8c35e8aa3f467b7930e
tomo_reb <tansansui87@gmail.com>
2021-09-05 10:51:25 +0900

    修正

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ for (let i = 1; i < 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz", i);
   } else if (i % 3 === 0) {
-    console.log(i);
+    console.log("Fizz", i);
   } else if (i % 5 === 0) {
-    console.log(i);
+    console.log("Buzz", i);
   }
 }
